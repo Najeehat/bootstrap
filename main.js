7ce135e5e0625e870d219feca711c10e69cf85f7
NAJEEHAT APEKE WAHAB <wahabnanjeehat@gmail.com>
2024-04-15 21:03:11 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
 
     //Stickey NavBar
-    $(window).scroll(dunction(){
-        if($(this).scrollTop()>0){
-            $('.navbar').addClass('bg-black');
-        }else{
-            $('.navbar').removeClass('bg-black'); 
-        }
-    })
-})
-
+    
